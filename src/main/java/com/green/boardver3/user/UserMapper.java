ab62e00d18f7ca2daad4d6e39592937a7eefa8cc
홍기윤 <wlwl753@naver.com>
2023-05-30 16:34:40 +0900

    yaml 세팅

--- src/main/java/com/green/boardver3/user/UserMapper.java
+++ src/main/java/com/green/boardver3/user/UserMapper.java
@@ -3,6 +3,7 @@ package com.green.boardver3.user;
 import com.green.boardver3.user.model.UserInsDto;
 import com.green.boardver3.user.model.UserLoginDto;
 import com.green.boardver3.user.model.UserLoginVo;
+import com.green.boardver3.user.model.UserPatchPwDto;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
@@ -10,7 +11,7 @@ public interface UserMapper {
 
     int insBoard(UserInsDto dto);
     UserLoginVo selUserByUid(UserLoginDto dto);
-
+    int updUserPw(UserPatchPwDto dto);
 
 
 
